c5af4fbdac6446af8793569bee41359de84bda79
PiotrKukula0 <piotr.kukula@student.po.edu.pl>
2023-03-21 17:07:32 +0100

    Project completion

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,30 @@
 import todoLogo from '../../assets/Logo.svg';
-import {AiOutlinePlusCircle} from 'react-icons/ai'
 import styles from './header.module.css';
+import { AiOutlinePlusCircle } from 'react-icons/ai';
+import { useState } from 'react';
 
-export function Header(){
-    return (
-        <header className={styles.header}>
-        <img src={todoLogo}/>
+export function Header({ handleAddTask }) {
+  const [title, setTitle] = useState('');
 
-        <form className={styles.newTaskForm}>
-            <input type="text" placeholder="Add a new task"/>
-            <button>
-            Create
-            <AiOutlinePlusCircle size={20}/>
-            </button>
-        </form>
-        </header>
-    )
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    handleAddTask(title);
+    setTitle('');
+  }
+
+  function onChangeTitle(event) {
+    setTitle(event.target.value);
+  }
+
+  return (
+    <header className={styles.header}>
+      <img src={todoLogo} />
+
+      <form onSubmit={handleSubmit} className={styles.newTaskForm}>
+        <input placeholder="Add a new task" type="text" onChange={onChangeTitle} value={title} />
+        <button>Create <AiOutlinePlusCircle size={20} /></button>
+      </form>
+    </header>
+  )
 }
